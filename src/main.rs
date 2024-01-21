7a8e8eb2901c1f84d1767196fa02c0be39df1f75
Savkin Nikita <sawkin.nik@gmail.com>
2024-01-21 14:25:16 +0400

    feat: echo telegram bot (closes #1)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use teloxide::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    pretty_env_logger::init();
+    log::info!("Starting echo bot...");
+
+    let bot = Bot::from_env();
+
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        match msg.text() {
+            Some(text) => bot.send_message(msg.chat.id, text).await?,
+            None => bot.send_message(msg.chat.id, "Send me plain text.").await?
+        };
+
+        Ok(())
+    }).await;
 }
